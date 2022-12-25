f67e494b5462f193bc0c7cb5bf2086454710bb79
Student User <student@workstation.lab.example.com>
2022-12-25 15:14:14 -0500

    updated app

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "A Change Is Coming!\n";
-print "sevil in web sayfasına hosgeldiniz\n";
+print "sevil in web sayfasi\n";
 ?>
